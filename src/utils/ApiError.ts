47b4fe42a35757e937b5bbf82ae2d02dbec0615b
Hamza <hamzashah.dev@gmail.com>
2024-12-13 16:23:27 +0500

    ts-ignroe added

--- src/utils/ApiError.ts
+++ src/utils/ApiError.ts
@@ -1,7 +1,9 @@
 export class ApiError extends Error {
   constructor(statusCode: number, message: string, isOperational = true, stack = '') {
     super(message);
+    // @ts-ignore
     this.statusCode = statusCode;
+    // @ts-ignore
     this.isOperational = isOperational;
     if (stack) {
       this.stack = stack;
