9ecd168784abae300da9c76e1149431dc5d4d1c1
Hamza <hamzashah.dev@gmail.com>
2024-12-17 19:03:56 +0500

    cloud update

--- src/utils/cloudinary.ts
+++ src/utils/cloudinary.ts
@@ -1,9 +1,10 @@
 import { v2 as cloudinary } from 'cloudinary';
 
-cloudinary.config({
+let cloud = cloudinary.config({
   cloud_name: 'daadraj4k',
   api_key: '885723874342741',
   api_secret: 'hOEQi88UIGhsdxsdtmaZsuEQs_Q',
   secure: true,
 });
-export default cloudinary;
+
+export default cloud;
